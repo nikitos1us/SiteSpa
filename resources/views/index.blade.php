47b2b6f88c209b8ab5e27fbba89f4bbca27394d6
Nik1 <nikitos1us@yandex.ru>
2021-03-08 20:47:34 +0300

    Настроил Mix(надо разобраться как ссылаться на объекты UIkit), добавил немного стиля

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,39 @@
-@extends('persons')
-
 @section('content')
-    {{ $test }}
+{{
+    $test
+}}
 @endsection
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="css/app.css" rel="stylesheet" type="text/css" />
+</head>
+</head>
+<body>
+<div class="content">
+    <h1 class="header-text">Спа-салон Афродита</h1>
+
+    <header>
+        <ul>
+            <li><a href="/">Главная</a></li>
+            <li><a href="/persons">Персонал</a></li>
+            <li><a href="/price">Прайс-лист</a></li>
+            <li><a href="/contacts">Контакты</a></li>
+        </ul>
+    </header>
+
+    <main>
+        <a>trululu</a>
+        <div>@yield('content')</div>
+
+    </main>
+
+
+
+</div>
+</body>
+<footer class="main-footer"> Fuck</footer>
+</html>
+
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css');
+
