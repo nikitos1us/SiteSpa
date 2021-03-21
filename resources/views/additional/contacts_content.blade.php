c5c8531251cdbcea5cd1fcf7b48dc34b3e5707f1
Nik1 <nikitos1us@yandex.ru>
2021-03-21 12:00:04 +0300

    Сделана страница Контакты. Добавить php-обработчик на кнопку.

--- resources/views/additional/contacts_content.blade.php
+++ resources/views/additional/contacts_content.blade.php
@@ -9,33 +9,84 @@
     </div>
 </div>
 
-<div class="uk-container">
-    <div class="uk-grid">
-        <div class="uk-align-center">
-            <div class="iframe">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2281.779146200876!2d36.591114851653614!3d55.11713904820721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46cad27e9f07d39d%3A0x2152205f4ea9482e!2z0KTQuNGC0L3QtdGBINCa0LvRg9CxINCh0KHQodCgINCe0LHQvdC40L3RgdC6!5e0!3m2!1sru!2sru!4v1616251276453!5m2!1sru!2sru"
-                    style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+<div class="uk-child-width-1-1@s uk-child-width-1-2@m uk-child-width-1-2@l" uk-grid>
+    <div>
+        <div class="uk-card uk-card-primary uk-align-center">
+            <div class="uk-card-body">
+                <p>
+                    <iframe class="iframe"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2281.779146200876!2d36.591114851653614!3d55.11713904820721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46cad27e9f07d39d%3A0x2152205f4ea9482e!2z0KTQuNGC0L3QtdGBINCa0LvRg9CxINCh0KHQodCgINCe0LHQvdC40L3RgdC6!5e0!3m2!1sru!2sru!4v1616251276453!5m2!1sru!2sru"
+                            style="border:0;"></iframe>
+                </p>
             </div>
+
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-primary uk-align-center">
+            <div class="uk-card-body">
+                <h1 class="uk-card-title">Контакты</h1>
+                <p>
+                    <span uk-icon="icon: mail"></span>
+                    <a class="contacts-list" href="mailto:example@mail.ru">example@mail.ru</a>
+                </p>
+                <p>
+                    <span uk-icon="icon: instagram"></span>
+                    <a class="contacts-list" href="https://www.instagram.com/afrodita_cccpobninsk/"
+                       target="_blank">@afrodita_cccpobninsk</a>
+                </p>
+                <p>
+                    <span uk-icon="icon: receiver"></span>
+                    <a class="contacts-list" href="tel:+79998882222">+79998882222</a>
+                </p>
+                <p>
+                    <span uk-icon="icon: whatsapp"></span>
+                    <a class="contacts-list" href="https://wa.me/+79998882222"
+                       target="_blank">+79998882222</a>
+                </p>
+
+
+            </div>
+
         </div>
     </div>
 </div>
 
+<div class="uk-grid-large uk-child-width-1-1@s uk-child-width-1-1@m uk-child-width-1-1@l" uk-grid>
+    <div>
+        <div class="uk-card uk-card-primary ">
+            <div class="uk-card-body">
+                <form>
+                    <fieldset class="uk-fieldset">
 
-<div class="uk-container">
-    <div class="uk-grid">
-        <div class=" uk-align-center ">
-            <div class="uk-card-primary uk-card-body">
-                <h1 class="uk-card-title">Default</h1>
-                <p>Мой <a href="#">компьютер</a>, должно быть, неисправен: каждый раз, когда я задаю ему
-                    неверный
-                    вопрос, он выдает мне неверный ответ.</p>
+                        <legend class="uk-legend">Форма обратной связи</legend>
 
+                        <div class="uk-margin">
+                            <input class="uk-input" type="text" placeholder="E-mail">
+                        </div>
+
+                        <div class="uk-margin">
+                            <select class="uk-select">
+                                <option> 01 </option>
+                                <option> 02 </option>
+                            </select>
+                        </div>
+
+                        <div class="uk-margin">
+                            <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
+                        </div>
+
+                        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+                            <label><input class="uk-radio" type="radio" name="radio2" checked> A</label>
+                            <label><input class="uk-radio" type="radio" name="radio2"> B</label>
+                        </div>
+
+                        <button class="uk-button uk-button-default">Отправить</button>
+
+                    </fieldset>
+                </form>
             </div>
         </div>
-
     </div>
 </div>
-
-
-
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -5,11 +5,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Спа-салон Афродита</title>
     @include('script_link')
-
 </head>
 
 <body>
-
 <div class="wrapper">
     <div class="content" uk-height-viewport="offset-bottom: 40px">
         <header>
@@ -26,10 +24,7 @@
         </main>
     </div>
 
-    @include('additional.footer')
 </div>
-
-
 </body>
 
 </html>
